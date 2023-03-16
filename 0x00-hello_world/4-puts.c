ac60b3606704f844dd2e68b5673747a85c9d6e6d
Joseph Mugi <jkamaumugi@gmail.com>
2023-03-16 16:21:14 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -starter printing line using put function
+ * Return: o
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
