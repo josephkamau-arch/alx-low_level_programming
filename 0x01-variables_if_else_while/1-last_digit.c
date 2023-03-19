03797e25a21d4598ba55a3adc8a503067f9adda7
Joseph Mugi <jkamaumugi@gmail.com>
2023-03-19 13:55:37 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,17 +1,13 @@
+#include <stdio.h>
 #include <stdlib.h>
-
 #include <time.h>
 
-#include <stdio.h>
-
 /**
- * main - Prints a text
- * Retrun: Always (Success)
- *
+ * main - Print Text
+ * Return: Always 0 (Success)
  */
 
 int main(void)
-
 {
 	int n, lastd;
 
