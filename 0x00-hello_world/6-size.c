3202afb786dee4dda908155ca1599a556c4810e8
Joseph Mugi <jkamaumugi@gmail.com>
2023-03-20 09:27:16 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main -  program that prints the size of various types on the computer it is compiled and run on
+ * main - print size of various type on the computer it is compiled and run on
  * return o
  */
 
