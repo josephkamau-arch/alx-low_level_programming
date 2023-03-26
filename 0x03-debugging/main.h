f54b27eac679d4f6ed8434c9b2a4f5cdc3d897ba
josephkamau-arch <jkamaumugi@gmail.com>
2023-03-26 11:27:06 -0700

    2-largest_number.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,8 +5,8 @@
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
-void print_remaining-day(int month, int day, int year);
+void print_remaining_day(int month, int day, int year);
 int convert_day(int month, int day);
-void print_times_table(int n):
+void print_times_table(int n);
 
 #endif
